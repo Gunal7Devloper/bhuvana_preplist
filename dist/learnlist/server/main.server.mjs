575a1769f8ce159b1112a664afad4fa3798ccf0c
Gunal7Devloper <114910887+Gunal7Devloper@users.noreply.github.com>
2025-06-19 16:54:32 +0000

    bootstrap added

--- dist/learnlist/server/main.server.mjs
+++ dist/learnlist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4I66NSAZ.mjs";import"./chunk-FDFQEVLA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TA6FKRMM.mjs";import"./chunk-FDFQEVLA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
